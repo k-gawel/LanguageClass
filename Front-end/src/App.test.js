2387c51bbf555fe2ac7335f63062fa521711f806
kamil <kamil.andy.gawel@gmail.com>
2022-08-10 12:53:32 +0200

    Deleted frontend files.

--- Front-end/src/App.test.js
+++
@@ -1,8 +0,0 @@
-import { render, screen } from '@testing-library/react';
-import StudyMaterials from './StudyMaterials';
-
-test('renders learn react link', () => {
-  render(<StudyMaterials />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
