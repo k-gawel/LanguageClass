d6698119d2b3a053fdc13c3334bc68f9877fb4d6
kamil <kamil.gawel.aplikacje@gmail.com>
2023-01-19 18:08:41 +0100

    Added repository for ExerciseEvaluation

--- answer-and-evaluation/src/main/java/language/contentandrepository/repository/impl/answerandevaluation/BaseExerciseEvaluationRepository.java
+++ answer-and-evaluation/src/main/java/language/contentandrepository/repository/impl/answerandevaluation/BaseExerciseEvaluationRepository.java
@@ -1,7 +1,22 @@
 package language.contentandrepository.repository.impl.answerandevaluation;
 
+import language.contentandrepository.model.domain.answerandevaluation.ExerciseEvaluation;
+import language.contentandrepository.persistence.dao.answerandevaluation.ExerciseEvaluationJpa;
+import language.contentandrepository.persistence.entity.answerandevaluation.ExerciseEvaluationEntity;
+import language.contentandrepository.persistence.mapper.answerandevaluation.ExerciseEvaluationMapper;
+import language.contentandrepository.repository.BaseContentRepository;
+import language.contentandrepository.repository.DomainCache;
+import language.contentandrepository.repository.answerandevalution.ExerciseEvaluationRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository(value = "exerciseEvaluationRepository")
-public class BaseExerciseEvaluationRepository {
+public class BaseExerciseEvaluationRepository extends BaseContentRepository<ExerciseEvaluation, ExerciseEvaluationEntity>
+                                              implements ExerciseEvaluationRepository {
+
+    public BaseExerciseEvaluationRepository(ExerciseEvaluationMapper entityToModelMapper,
+                                            ExerciseEvaluationJpa jpaRepository,
+                                            ExerciseEvaluationJpa.ID idsJpaRepository) {
+        super(ExerciseEvaluation.class, entityToModelMapper, new DomainCache<>(), jpaRepository, idsJpaRepository);
+    }
+
 }
