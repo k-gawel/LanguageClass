df8394f015888b62aceb62189a514b5beec2c56e
kamil <kamil.gawel.aplikacje@gmail.com>
2023-01-19 18:50:34 +0100

    AppUserRepository returns null in case there's no results

--- answer-and-evaluation/src/main/java/language/contentandrepository/repository/user/AppUserRepository.java
+++ answer-and-evaluation/src/main/java/language/contentandrepository/repository/user/AppUserRepository.java
@@ -1,7 +1,18 @@
 package language.contentandrepository.repository.user;
 
+import language.contentandrepository.model.DomainID;
 import language.contentandrepository.model.domain.user.AppUser;
 import language.contentandrepository.repository.ContentRepository;
 
 public interface AppUserRepository extends ContentRepository<AppUser> {
+
+    @Override
+    default AppUser getById(DomainID<AppUser> id) {
+        return findById(id).orElse(null);
+    }
+
+    @Override
+    default AppUser getById(String id) {
+        return findById(id).orElse(null);
+    }
 }
