48ee376648f88314e54fa3dbc61f979d11247f20
kamil <kamil.gawel.aplikacje@gmail.com>
2023-01-22 13:41:39 +0100

    Added textbook elements query and criteria.

--- answer-and-evaluation/src/main/java/language/graphql/textbook/query/TextbookElementsQuery.java
+++ answer-and-evaluation/src/main/java/language/graphql/textbook/query/TextbookElementsQuery.java
@@ -1,12 +1,15 @@
 package language.graphql.textbook.query;
 
 import com.coxautodev.graphql.tools.GraphQLQueryResolver;
+import language.contentandrepository.criteria.textbook.ChapterCriteria;
+import language.contentandrepository.criteria.textbook.ExampleCriteria;
+import language.contentandrepository.criteria.textbook.ExerciseCriteria;
 import language.contentandrepository.criteria.textbook.TextbookCriteria;
 import language.contentandrepository.model.domain.textbook.Chapter;
+import language.contentandrepository.model.domain.textbook.Example;
+import language.contentandrepository.model.domain.textbook.Exercise;
 import language.contentandrepository.model.domain.textbook.Textbook;
-import language.contentandrepository.repository.textbook.ChapterRepository;
-import language.contentandrepository.repository.textbook.TextbookElementsRepository;
-import language.contentandrepository.repository.textbook.TextbookRepository;
+import language.contentandrepository.repository.textbook.*;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.access.prepost.PostAuthorize;
 import org.springframework.security.access.prepost.PostFilter;
@@ -22,6 +25,8 @@ public class TextbookElementsQuery implements GraphQLQueryResolver {
     private final TextbookElementsRepository textbookElementsRepository;
     private final TextbookRepository textbookRepository;
     private final ChapterRepository chapterRepository;
+    private final ExampleRepository exampleRepository;
+    private final ExerciseRepository exerciseRepository;
 
     @PreAuthorize("hasAnyAuthority('TEACHER', 'STUDENT')")
     @PostAuthorize("@textbookAccessFilter.hasAccess(authentication, returnObject)")
@@ -36,8 +41,39 @@ public class TextbookElementsQuery implements GraphQLQueryResolver {
     }
 
     @PreAuthorize("isAuthenticated()")
-    @PostAuthorize("@textbookAccessFilter.hasAccess")
+    @PostAuthorize("@textbookAccessFilter.hasAccess(authentication, returnObject)")
     public Chapter getChapter(String id) {
         return chapterRepository.getById(id);
     }
+    
+    @PreAuthorize("isAuthenticated()")
+    @PostFilter("@textbookAccessFilter.hasAccess(authentication, filterObject)")
+    public List<Chapter> findChapter(ChapterCriteria criteria) {
+        return textbookElementsRepository.find(criteria);
+    }
+    
+    @PreAuthorize("isAuthenticated()")
+    @PostAuthorize("@textbookAccessFilter.hasAccess(authentication, returnObject)")
+    public Example getExample(String id) {
+        return exampleRepository.getById(id);
+    }
+    
+    @PreAuthorize("isAuthenticated()")
+    @PostFilter("@textbookAccessFilter.hasAccess(authentication, filterObject)")
+    public List<Example> findExample(ExampleCriteria criteria) {
+        return textbookElementsRepository.find(criteria);
+    }
+    
+    @PreAuthorize("isAuthenticated()")
+    @PostAuthorize("@textbookAccessFilter.hasAccess(authentication, returnObject)")
+    public Exercise getExercise(String id) {
+        return exerciseRepository.getById(id);
+    }
+
+    @PreAuthorize("isAuthenticated()")
+    @PostFilter("@textbookAccessFilter.hasAccess(authentication, filterObject)")
+    public List<Exercise> findExercise(ExerciseCriteria criteria) {
+        return textbookElementsRepository.find(criteria);
+    }
+
 }
