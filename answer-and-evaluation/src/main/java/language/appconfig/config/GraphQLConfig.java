15f0fe7bf2ba1ee74832007e954f25991a172549
kamil <kamil.gawel.aplikacje@gmail.com>
2023-01-22 13:42:00 +0100

    Added Date scalar.

--- answer-and-evaluation/src/main/java/language/appconfig/config/GraphQLConfig.java
+++ answer-and-evaluation/src/main/java/language/appconfig/config/GraphQLConfig.java
@@ -1,6 +1,7 @@
 package language.appconfig.config;
 
 import com.coxautodev.graphql.tools.SchemaParserDictionary;
+import graphql.schema.*;
 import language.contentandrepository.model.domain.answerandevaluation.ExerciseAnswer;
 import language.contentandrepository.model.domain.question.AnswerAQuestion;
 import language.contentandrepository.model.domain.question.ChooseAWord;
@@ -11,7 +12,10 @@ import language.contentandrepository.model.domain.user.Teacher;
 import org.springframework.context.annotation.Bean;
 import org.springframework.stereotype.Component;
 
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.List;
+import java.util.Locale;
 
 @Component
 public class GraphQLConfig {
@@ -28,5 +32,36 @@ public class GraphQLConfig {
         return new SchemaParserDictionary()
             .add(classes);
     }
+
+    @Bean
+    public GraphQLScalarType dateScalarType() {
+
+        var coercing = new Coercing() {
+            @Override
+            public Object serialize(Object o) throws CoercingSerializeException {
+                SimpleDateFormat formatter = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss.SSSXXX", Locale.ENGLISH);
+                return formatter.format((Date)o);
+            }
+
+            @Override
+            public Object parseValue(Object o) throws CoercingParseValueException {
+                return o;
+            }
+
+            @Override
+            public Object parseLiteral(Object o) throws CoercingParseLiteralException {
+                if (o==null){
+                    return null;
+                }
+                return o.toString();
+            }
+
+        };
+
+        return new GraphQLScalarType("Date", "DateTime", coercing);
+
+    }
+
+
 
 }
