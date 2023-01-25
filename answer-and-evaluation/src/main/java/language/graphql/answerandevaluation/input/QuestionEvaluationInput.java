215691583ee97d48ca19236a22b12e7a7e552550
kamil <kamil.gawel.aplikacje@gmail.com>
2023-01-25 12:26:18 +0100

    New binders and inputs.

--- answer-and-evaluation/src/main/java/language/graphql/answerandevaluation/input/QuestionEvaluationInput.java
+++ answer-and-evaluation/src/main/java/language/graphql/answerandevaluation/input/QuestionEvaluationInput.java
@@ -3,6 +3,7 @@ package language.graphql.answerandevaluation.input;
 import java.util.List;
 
 public record QuestionEvaluationInput(
+        String id,
         String answer,
         String author,
         List<String> comments,
