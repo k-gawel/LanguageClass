ed36f42aee881c681d742f88946c1a3fe793a04e
kamil <kamil.gawel.aplikacje@gmail.com>
2023-01-25 12:25:49 +0100

    New binders and inputs.

--- answer-and-evaluation/src/main/java/language/service/service/answerandevaluation/inputs/ExerciseEvaluationCreateInput.java
+++ answer-and-evaluation/src/main/java/language/service/service/answerandevaluation/inputs/ExerciseEvaluationCreateInput.java
@@ -1,5 +1,16 @@
 package language.service.service.answerandevaluation.inputs;
 
+import language.contentandrepository.model.domain.answerandevaluation.ExerciseAnswer;
+import language.contentandrepository.model.domain.answerandevaluation.QuestionEvaluation;
+import language.contentandrepository.model.domain.user.Teacher;
+
+import java.util.List;
+
 public record ExerciseEvaluationCreateInput(
-) {
+        ExerciseAnswer exerciseAnswer,
+        Teacher author,
+        List<QuestionEvaluation> questionEvaluations,
+        int rating,
+        String comment
+        ) {
 }
