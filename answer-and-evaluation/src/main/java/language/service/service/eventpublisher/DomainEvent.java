8d29e8716ba17006efcb21d854597ed0b60bffbc
kamil <kamil.gawel.aplikacje@gmail.com>
2023-01-25 12:21:23 +0100

    Added new event and fixed wrong input types.

--- answer-and-evaluation/src/main/java/language/service/service/eventpublisher/DomainEvent.java
+++ answer-and-evaluation/src/main/java/language/service/service/eventpublisher/DomainEvent.java
@@ -9,12 +9,10 @@ import language.contentandrepository.model.domain.question.FillAWord;
 import language.contentandrepository.model.domain.textbook.Chapter;
 import language.contentandrepository.model.domain.textbook.Exercise;
 import language.contentandrepository.model.domain.textbook.Textbook;
-import language.graphql.answerandevaluation.input.ExerciseAnswerInput;
 import language.graphql.answerandevaluation.input.QuestionEvaluationInput;
 import language.graphql.question.input.ChooseAWordInput;
 import language.graphql.question.input.FillAWordInput;
 import language.graphql.textbook.input.ChapterUpdateInput;
-import language.graphql.textbook.input.ModifyContentInput;
 import language.service.service.answerandevaluation.inputs.*;
 import language.service.service.textbook.inputs.*;
 import org.hibernate.criterion.Example;
@@ -24,7 +22,7 @@ import java.util.List;
 public enum DomainEvent {
 
     TEXTBOOK_CREATED("textbook.created", TextbookCreateInput.class, Textbook.class),
-    TEXTBOOK_CONTENT_MODIFIED("textbook.content_modified", ModifyContentInput.class, List.class),
+    TEXTBOOK_CONTENT_MODIFIED("textbook.content_modified", TextbookContentModifyInput.class, List.class),
     TEXTBOOK_ACCESS_MODIFIED("textbook.access_modified", TextbookModifyAccessInput.class, Boolean.class),
 
     CHAPTER_CONTENT_MODIFIED("chapter.content_modified", ChapterContentModifyInput.class, List.class),
@@ -34,9 +32,9 @@ public enum DomainEvent {
     EXAMPLE_CREATED("example.created", ExampleCreateInput.class, Example.class),
     EXAMPLE_MODIFIED("example.modified", ExampleUpdateInput.class, Example.class),
 
-    EXERCISE_CREATED("exercise.created", ExerciseCreateInput.class, Exercise.class),
-    EXERCISE_UPDATED("exercise.updated", ExerciseUpdateInput.class, Exercise.class),
-    EXERCISE_CONTENT_MODIFIED("exercise.content_modified", ExerciseModifyContentInput.class, List.class),
+    EXERCISE_CREATED("exerciseAnswer.created", ExerciseCreateInput.class, Exercise.class),
+    EXERCISE_UPDATED("exerciseAnswer.updated", ExerciseUpdateInput.class, Exercise.class),
+    EXERCISE_CONTENT_MODIFIED("exerciseAnswer.content_modified", ExerciseModifyContentInput.class, List.class),
 
     QUESTION_CREATED_CHOOSE_A_WORD("question.created", ChooseAWordInput.class, ChooseAWord.class),
     QUESTION_CREATED_FILL_A_WORD("question.created", FillAWordInput.class, FillAWord.class),
@@ -47,7 +45,7 @@ public enum DomainEvent {
     QUESTION_EVALUATION_CREATED("question_evaluation.created", QuestionEvaluationInput.class, QuestionEvaluation.class),
 
     EXERCISE_ANSWER_CREATED("exercise_answer.created", ExerciseAnswerCreateInput.class, ExerciseAnswer.class),
-    EXERCISE_ANSWER_UPDATED("exercise_answer.updated", ExerciseAnswerInput.class, ExerciseAnswer.class),
+    EXERCISE_ANSWER_UPDATED("exercise_answer.updated", ExerciseAnswerUpdateInput.class, ExerciseAnswer.class),
 
     EXERCISE_EVALUATION_CREATED("exercise_evaluation.created", ExerciseEvaluationCreateInput.class, ExerciseEvaluation.class),
     EXERCISE_EVALUATION_UPDATED("exercise_evaluation.updated", ExerciseEvaluationUpdateInput.class, ExerciseEvaluation.class);
